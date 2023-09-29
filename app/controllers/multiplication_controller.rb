7bec81c326cd1945fc33a98b7d719064ba4e6579
Ricardo Jimenez <124076987+RJDPI@users.noreply.github.com>
2023-09-29 23:07:06 +0000

    finished

---
+++ app/controllers/multiplication_controller.rb
@@ -0,0 +1,14 @@
+class MultiplicationController < ApplicationController
+  def multiplication
+    render({ :template => "multiplication_templates/multiplication_form" })
+  end
+
+  def multiply_results
+    @first_num = params.fetch("first_number").to_f
+    @second_num = params.fetch("second_number").to_f
+
+    @result =  @first_num * @second_num
+
+    render({ :template => "multiplication_templates/multiply_results" })
+  end
+end
